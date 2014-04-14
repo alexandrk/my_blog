8e7fbf47c86de7aa6df9d501420f811947d97fce
Alexander Kazakov <alexander@kazakov.mobi>
2014-04-13 21:13:24 -0700

    Adds posts/users associations. Tag styling updates.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410193402) do
+ActiveRecord::Schema.define(version: 20140413211543) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20140410193402) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
