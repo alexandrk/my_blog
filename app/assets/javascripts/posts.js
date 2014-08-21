c793274489e45afd6378a57c8c073950c79f78e6
Alexander Kazakov <alexander@kazakov.mobi>
2014-08-21 15:21:14 -0700

    Switch posts display view functionality. --FIXED 	- added event.preventDefault to js code

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,6 +1,6 @@
 $(function(){
 
-	$('.nav').on('click', '.btn-secondary', function(){
+	$('.nav').on('click', '.btn-secondary', function(event){
 
 		if ( $(this).data('view-type') === "list-view" ){
 			$('.posts').children('.panel').addClass('x100percent');
@@ -9,6 +9,8 @@ $(function(){
 		{
 			$('.posts').children('.panel').removeClass('x100percent');
 		}
+
+		event.preventDefault();
 
 	});
 
